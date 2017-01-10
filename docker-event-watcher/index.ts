6c9ba69f8dab008338ece03d0d043a22cdb322dc
Michael Irwin <mikesir87@gmail.com>
2017-01-10 11:39:05 -0500

    Updated event watcher to seed containers upon startup

--- docker-event-watcher/index.ts
+++ docker-event-watcher/index.ts
@@ -2,44 +2,66 @@ import Dockerode from 'dockerode-ts';
 const dockerClient = new Dockerode();
 import { publisher } from './publisher';
 import { DeploymentUpdateEvent } from "../shared/events";
+import {ContainerInfo} from "dockerode-ts";
 
-let options = {
+const labelArray = [
+  'deployment.name', 'deployment.url',
+  'deployment.issue.identifier', 'deployment.issue.url',
+  'deployment.vcs.ref'
+];
+
+const eventStreamOptions = {
   filters: {
     type: ['container'],
-    event: ['create', 'destroy'],
-    label: [
-      'deployment.name', 'deployment.url',
-      'deployment.issue.identifier', 'deployment.issue.url',
-      'deployment.vcs.ref'
-    ]
+    event: ['die', 'start'],
+    label: labelArray
   }
 };
 
-dockerClient.getEvents(options, function(err, stream) {
+const listOptions = {
+  filters: {
+    label: labelArray
+  }
+};
+
+const processContainer = (status : 'UP' | 'DOWN', labels : any) => {
+  let message : DeploymentUpdateEvent = {
+    name : labels['deployment.name'],
+    status,
+    url : labels['deployment.url'],
+    issue : {
+      identifier: labels['deployment.issue.identifier'],
+      url : labels['deployment.issue.url']
+    },
+    lastCommit : {
+      ref : labels['deployment.vcs.ref'],
+    }
+  };
+  publisher.publishMessage(message);
+};
+
+dockerClient.listContainers(listOptions, (err, containers : ContainerInfo[]) => {
   if (err) return console.error(err.message, err);
+
+  containers.forEach((container : ContainerInfo) => {
+    let labels : any = container.Labels;
+    processContainer("UP", labels);
+  });
+});
+
+
+dockerClient.getEvents(eventStreamOptions, function(err, stream) {
+  if (err) return console.error(err.message, err);
+  console.log("Listening for Docker events");
   stream.setEncoding('utf8');
 
   stream.on('data', function(chunk) {
     let data = JSON.parse(chunk.toString());
-    let attributes = data.Actor.Attributes;
-
-    let message : DeploymentUpdateEvent = {
-      name : attributes['deployment.name'],
-      status : (data.Action == 'create') ? 'UP' : 'DOWN',
-      url : attributes['deployment.url'],
-      issue : {
-        identifier: attributes['deployment.issue.identifier'],
-        url : attributes['deployment.issue.url']
-      },
-      lastCommit : {
-        ref : attributes['deployment.vcs.ref'],
-      }
-    };
-    publisher.publishMessage(message);
+    console.log("Received event", chunk);
+    processContainer((data.Action == 'start') ? 'UP' : 'DOWN', data.Actor.Attributes);
   });
 
   stream.on('close', function() {
     console.warn("Connection to Docker daemon has been lost");
   });
 });
-
